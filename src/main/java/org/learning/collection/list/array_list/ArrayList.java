fcbd60a521d8955548e10677a439d4f5e7daf11d
Anudeepk1 <anudeepkone@gmail.com>
2024-11-16 13:50:29 +0530

    Implement methods for DoublyLinkedList

--- src/main/java/org/learning/collection/list/array_list/ArrayList.java
+++ src/main/java/org/learning/collection/list/array_list/ArrayList.java
@@ -2,5 +2,79 @@ package org.learning.collection.list.array_list;
 
 import org.learning.collection.list.List;
 
-public class ArrayList {
+public class ArrayList<E> implements List<E>{
+    @Override
+    public void get(int index) {
+
+    }
+
+    @Override
+    public void set(E data, int index) {
+
+    }
+
+    @Override
+    public void add(E data) {
+
+    }
+
+    @Override
+    public void addFirst(E data) {
+
+    }
+
+    @Override
+    public void addAtIndex(E data, int index) {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public boolean contains(E value) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public E[] toArray() {
+        return null;
+    }
+
+    @Override
+    public void removeFirst() {
+
+    }
+
+    @Override
+    public void removeAtIndex(int index) {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
+
+    @Override
+    public void subList(int fromIndex, int toIndex) {
+
+    }
 }
--- src/main/java/org/learning/collection/list/linked_list/doubly_linked_list/DoublyLinkedList.java
+++ src/main/java/org/learning/collection/list/linked_list/doubly_linked_list/DoublyLinkedList.java
@@ -1,4 +1,98 @@
 package org.learning.collection.list.linked_list.doubly_linked_list;
 
-public class DoublyLinkedList {
+import org.learning.collection.list.linked_list.LinkedList;
+
+public class DoublyLinkedList<E> extends LinkedList<E> {
+    @Override
+    public void add(E data) {
+        Node<E> node = new Node<>(data);
+        if(head == null){
+            head = node;
+            tail = node;
+        } else {
+            tail.next = node;
+            node.prev = tail;
+            tail = node;
+        }
+        size++;
+
+    }
+
+    @Override
+    public void addFirst(E data) {
+        Node<E> node = new Node<>(data);
+        if(head == null){
+            head = node;
+            tail = node;
+        } else {
+            head.prev = node;
+            node.next = head;
+            head = node;
+        }
+        size++;
+    }
+
+    @Override
+    public void addAtIndex(E data, int index) {
+        if(index == 0) addFirst(data);
+        else if (index == size) add(data);
+        else {
+            if(index < 0 || index < size){
+                System.out.println("Index out of bound");
+                return;
+            }
+            Node<E> currentNode = head;
+            Node<E> node = new Node<>(data);
+            for(int i=0; i<index ; i++){
+               currentNode = currentNode.next;
+            }
+            node.next = currentNode.next;
+            currentNode.next.prev = node;
+
+            currentNode.next = node;
+            node.prev = currentNode;
+            size++;
+        }
+
+    }
+
+    @Override
+    public void remove() {
+        if(head == null){
+            System.out.println("List is empty");
+            return;
+        }
+        tail = tail.prev;
+        tail.next = null;
+        size--;
+    }
+
+    @Override
+    public void removeFirst() {
+        if(head == null){
+            System.out.println("List is empty");
+            return;
+        }
+        head = head.next;
+        head.prev = null;
+        size--;
+    }
+
+    @Override
+    public void removeAtIndex(int index) {
+        if(index == 0) removeFirst();
+        else if(index == size) remove();
+        else {
+            if(index < 0 || index < size){
+                System.out.println("Index out of bound");
+                return;
+            }
+            Node<E> currentNode = head;
+            for(int i=0; i<index ; i++){
+                currentNode = currentNode.next;
+            }
+            currentNode.next =  currentNode.next.next;
+            currentNode.next.prev = currentNode;
+        }
+    }
 }
